0f7e0fefc6bc6126c8de714c1fdb2e8da0895a21
C.W. Chien <dppss92132@gmail.com>
2021-12-05 22:35:43 +0800

    Change app name and add build/exec shell script
    The original Producer class name is conflicting with real Producer<K,V>
    class. For building, normally people use Maven of Ant but I think in
    this project is simple enough to just use shell scipt.
    
    Note:
    * path after -cp is file path not directory path
    * You can not import whole clients.producer. You should specify one
      class inside. Or just producer.* (idk why yet)

--- FastFeeder.java
+++ FastFeeder.java
@@ -1,4 +1,6 @@
-class Producer {
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+class FastFeeder {
     public static void main(String args[]) {
         for (int i = 0; i < args.length; i++)
             System.out.println(args[i]);
---
+++ build.sh
@@ -0,0 +1,1 @@
+javac ./FastFeeder.java -cp /home/idoleat/Documents/NCKU/kafka/kafka_2.13-3.0.0/libs/kafka-clients-3.0.0.jar
