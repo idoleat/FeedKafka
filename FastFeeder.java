fd87324610615dd69063aa19a2cadb3fee052dc9
C.W. Chien <dppss92132@gmail.com>
2021-12-31 12:26:28 +0800

    Parse args and construct records to send
    record size is defined in header :thinking:

--- FastFeeder.java
+++ FastFeeder.java
@@ -1,41 +1,75 @@
 import java.util.Properties;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.List;
+import java.util.Arrays;
+import java.lang.Integer;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.header.Header;
 
 class FastFeeder {
     public static void main(String args[]) {
-        String topicName = "test";
+        String brokers = "";
+        String topic = "";
+        int records = 0;
+        int recordSize = 0;
 
-        // create instance for properties to access producer configs
+        // a bad way to parse argument for sure
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+            case "--brokers":
+                brokers = args[i + 1];
+                i += 1;
+                break;
+            case "--topic":
+                topic = args[i + 1];
+                i += 1;
+                break;
+            case "--records":
+                records = Integer.parseInt(args[i + 1]);
+                i += 1;
+                break;
+            case "--recordSize":
+                recordSize = Integer.parseInt(args[i + 1]);
+                i += 1;
+                break;
+            }
+        }
+
+        final int size = recordSize;
+
         Properties props = new Properties();
-
-        // Assign localhost id
-        props.put("bootstrap.servers", "0.0.0.0:16864");
-
-        // Set acknowledgements for producer requests.
-        props.put("acks", "all");
-
-        // If the request fails, the producer can automatically retry,
-        props.put("retries", 0);
-
-        // Specify buffer size in config
-        props.put("batch.size", 16384);
-
-        // Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
-
-        // The buffer.memory controls the total amount of memory available to the
-        // producer for buffering.
-        props.put("buffer.memory", 33554432);
+        props.put("bootstrap.servers", brokers);
+        props.put("acks", "1"); // Set acknowledgements for producer requests. all is SLOWWWW
+        props.put("retries", 0); // If the request fails, the producer can automatically retry
+        props.put("batch.size", 16384); // Specify buffer size in config
+        props.put("linger.ms", 1); // Reduce the no of requests less than 0
+        props.put("buffer.memory", 33554432); // The buffer.memory controls the total amount of memory available to the
+                                              // producer for buffering.
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+        Header data = new Header() {
+            @Override
+            public String key() {
+                return "";
+            }
+
+            @Override
+            public byte[] value() {
+                return new byte[size];
+            }
+        };
+
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
 
-        for (int i = 0; i < 10; i++)
-            producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i), Integer.toString(i)));
+        for (int i = 0; i < records; i++) {
+            String key, value;
+            key = String.format("key-%06d", i);
+            value = String.format("value-%06d", i);
+            producer.send(new ProducerRecord<String, String>(topic, null, key, value, Arrays.asList(data)));
+        }
         System.out.println("Message sent successfully");
+
         producer.close();
     }
 }
